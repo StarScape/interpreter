cd571eced72252cd5415436f999720b639f97d6d
Jack <mail@jackarrington.com>
2019-05-01 09:33:58 -0400

    Fin :)

--- interpreter.hs
+++ interpreter.hs
@@ -37,11 +37,13 @@ data Expr = Const Val
           | Minus Expr Expr
           | Times Expr Expr
           | Greater Expr Expr
+          deriving Show
 
 data Command = Assign Variable Expr
              | Seq Command Command
              | Cond Expr Command Command
              | While Expr Command
+             deriving Show
 
 -- Initial store for our program (everything is 0)
 initial :: Store
@@ -88,6 +90,111 @@ data Token = Ident String
            | Number Int
            | Symbol String
            deriving Show
+
+type Parser a = [Token] -> Maybe (a, [Token])
+
+-- [Token] -> Maybe (Int, [Token])
+number   :: Parser Val
+number (Number n : s) = Just (n, s)
+number _              = Nothing
+
+-- [Token] -> Maybe (String, [Token])
+variable :: Parser Variable
+variable (Ident i : is) = Just (i, is)
+variable _              = Nothing
+
+-- String -> [Token] -> Maybe (String, [Token])
+literal :: String -> Parser String
+literal s1 (Symbol s2 : s) | s1 == s2  = Just (s2, s)
+                           | otherwise = Nothing
+literal _ _                            = Nothing
+
+-- expr ::= aexp [“>” aexp]
+expr = (aexp <&> optional (literal ">" <&> aexp)) `modify` optGreater
+  where
+    -- Generate Expr for greater than
+    optGreater (e1, []) = e1
+    optGreater (e1, [(_, e2)]) = Greater e1 e2
+    optGreater _ = error "impossible"
+
+-- aexp ::= bexp [“−” aexp]
+aexp = (bexp <&> optional (literal "-" <&> aexp)) `modify` optMinus
+  where
+    -- Generate Expr for subtraction
+    optMinus (e1, []) = e1
+    optMinus (e1, [(_, e2)]) = Minus e1 e2
+    optMinus _ = error "impossible"
+
+-- bexp ::= cexp [“*” bexp]
+bexp = (cexp <&> optional (literal "*" <&> bexp)) `modify` optTimes
+  where
+    -- Generate Expr for multiplication
+    optTimes (e1, []) = e1
+    optTimes (e1, [(_, e2)]) = Times e1 e2
+    optTimes _ = error "impossible"
+
+-- cexp ::= "(" expr ")"
+--      | number
+--      | variable
+cexp = (literal "(" <&> expr <&> literal ")") `modify` unparenth
+        <|> (number `modify` Const)
+        <|> (variable `modify` Var)
+        where unparenth ((_, e), _) = e
+
+-- command ::= unitcom [“;” command]
+command = (unitcom <&> optional (literal ";" <&> command)) `modify` optSeq
+  where 
+    -- Generate the Command for sequential statements
+    optSeq (c1, []) = c1
+    optSeq (c1, [(_, c2)]) = Seq c1 c2
+    optSeq _ = error "impossible"
+
+unitcom = whilecom <|> (ifcom <|> assign)
+
+-- Parser for WHILE statements
+whilecom = (literal "WHILE" <&> expr <&> literal "DO" <&> command <&> literal "END") `modify` mkWhileNode
+  where
+    -- Generate the Command for WHILE statements
+    mkWhileNode :: ((((String, Expr), String), Command), String) -> Command
+    mkWhileNode ((((_, e), _), c), _) = While e c
+
+-- Parser for IF statements
+ifcom = (literal "IF" <&> expr <&> literal "THEN" <&> command <&> literal "ELSE" <&> command <&> literal "ENDIF") `modify` mkIfNode
+  where
+    -- Generate the Command for IF statements
+    mkIfNode :: ((((((String, Expr), String), Command), String), Command), String) -> Command
+    mkIfNode ((((((_, e), _), ifc), _), elsec), _) = Cond e ifc elsec
+
+-- Parser for ASSIGNMENT statements
+assign = (variable <&> literal ":=" <&> expr) `modify` mkAssignNode
+  where
+    -- Generate the Command for ASSIGNMENT statements
+    mkAssignNode :: ((Variable, String), Expr) -> Command
+    mkAssignNode ((v,_), e) = Assign v e
+
+lit :: Token -> String
+lit (Ident s) = s ++ " "
+lit (Symbol s) = s ++ " "
+lit (Number s) = show s ++ " "
+
+report :: Maybe (a, [Token]) -> a
+report Nothing = error "Parse error"
+report (Just(c, [])) = c
+report (Just(c, xs)) = error (stringwith
+                                ("Syntax error \n Unparsed:-\n",
+                                 " ",
+                                 "\n")
+                                 (map lit xs))
+
+stringwith (front, sep, back) ls =
+  let sepback [] = back
+      sepback [a] = a ++ back
+      sepback (a:xs) = a ++ sep ++ sepback xs
+  in
+    front ++ sepback ls
+
+mainParser :: [Token] -> Command
+mainParser = report . command
 
 -- ====================================
 -- |            Lexer Code            |
--- parser.hs
+++ parser.hs
@@ -1,13 +1,3 @@
--- ======================================
--- |           Parser Code              |
--- ======================================
-
--- Types needed for parser
-data Token = Ident String
-           | Number Int
-           | Symbol String
-           deriving Show
-
 type Parser a = [Token] -> Maybe (a, [Token])
 
 -- [Token] -> Maybe (Int, [Token])
@@ -26,79 +16,65 @@ literal s1 (Symbol s2 : s) | s1 == s2  = Just (s2, s)
                            | otherwise = Nothing
 literal _ _                            = Nothing
 
--- -- expr ::= aexp [“>” aexp]
--- expr = (aexp <&> optional (literal ">" <&> aexp)) `modify` optGreater
+-- expr ::= aexp [“>” aexp]
+expr = (aexp <&> optional (literal ">" <&> aexp)) `modify` optGreater
 
--- optGreater (e1, []) = e1
--- optGreater (e1, [(_, e2)]) = Greater e1 e2
--- optGreater _ = error "impossible"
+optGreater (e1, []) = e1
+optGreater (e1, [(_, e2)]) = Greater e1 e2
+optGreater _ = error "impossible"
 
--- -- aexp ::= bexp [“−” aexp]
--- aexp = (bexp <&> optional (literal "-" <&> aexp)) `modify` optMinus
+-- aexp ::= bexp [“−” aexp]
+aexp = (bexp <&> optional (literal "-" <&> aexp)) `modify` optMinus
 
--- optMinus (e1, []) = e1
--- optMinus (e1, [(_, e2)]) = Minus e1 e2
--- optMinus _ = error "impossible"
+optMinus (e1, []) = e1
+optMinus (e1, [(_, e2)]) = Minus e1 e2
+optMinus _ = error "impossible"
 
 -- bexp ::= cexp [“*” bexp]
--- bexp = (cexp <&> optional (literal "*" <&> bexp)) `modify` optTimes
+bexp = (cexp <&> optional (literal "*" <&> bexp)) `modify` optTimes
 
 -- (((String, Expr), String), [(String, Expr)])
--- optTimes (((_, e1), _), []) = e1
--- optTimes (((_, e1), _), [(_, e2)]) = Times e1 e2
--- optTimes _ = error "impossible"
+optTimes (((_, e1), _), []) = e1
+optTimes (((_, e1), _), [(_, e2)]) = Times e1 e2
+optTimes _ = error "impossible"
 
 -- cexp ::= "(" expr ")"
 --      | number
 --      | variable
--- Parser Variable == [Token] -> (Variable, )
 cexp = (literal "(" <&> expr <&> literal ")") `modify` unparenth
        <|> number
        <|> variable
 
--- Parser (Parser (String, Expr))
-
 -- unparenth :: ((String, Expr), String) -> Expr
 unparenth ((_, e), _) = e
 
--- :t number = Parser Val = [Token] -> (String, [Token])
+command ::= unitcom [“;” command]
+command = (unitcom <&> optional (literal ";" <&> command)) `modify` optSeq
 
--- constify :: (Int, [Token]) -> Expr
-constify n = Const n
+-- Generate the Command for sequential statements
+optSeq (c1, []) = c1
+optSeq (c1, [(_, c2)]) = Seq c1 c2
+optSeq _ = error "impossible"
 
--- :t variable = Parser Variable = [Token] -> (Int, [Token])
+unitcom = whilecom <|> (ifcom <|> assign)
 
--- varify :: (String, [Token]) -> Expr
--- varify (name, _) = Var name
-varify name = Var name
+-- Parser for WHILE statements
+whilecom = (literal "WHILE" <&> expr <&> literal "DO" <&> command <&> literal "END") `modify` mkWhileNode
 
--- command ::= unitcom [“;” command]
--- command = (unitcom <&> optional (literal ";" <&> command)) `modify` optSeq
+-- Generate the Command for WHILE statements
+mkWhileNode :: (((String, Expr), String), Command) -> Command
+mkWhileNode ((((_, e), _), c), _) = While e c
 
--- -- Generate the Command for sequential statements
--- optSeq (c1, []) = c1
--- optSeq (c1, [(_, c2)]) = Seq c1 c2
--- optSeq _ = error "impossible"
+-- Parser for IF statements
+ifcom = (literal "IF" <&> expr <&> literal "THEN" <&> command <&> literal "ELSE" <&> command <&> literal "ENDIF") `modify` mkIfNode
 
--- unitcom = whilecom <|> (ifcom <|> assign)
+-- Generate the Command for IF statements
+mkIfNode :: ((((((String, Expr), String), Command), String), Command), String) -> Command
+mkIfNode ((((((_, e), _), ifc), _), elsec), _) = Cond e ifc elsec
 
--- -- Parser for WHILE statements
--- whilecom = (literal "WHILE" <&> expr <&> literal "DO" <&> command <&> literal "END") `modify` mkWhileNode
+-- Parser for ASSIGNMENT statements
+assign = (variable <&> literal ":=" <&> expr) `modify` mkAssignNode
 
--- -- Generate the Command for WHILE statements
--- mkWhileNode :: (((String, Expr), String), Command) -> Command
--- mkWhileNode ((((_, e), _), c), _) = While e c
-
--- -- Parser for IF statements
--- ifcom = (literal "IF" <&> expr <&> literal "THEN" <&> command <&> literal "ELSE" <&> command <&> literal "ENDIF") `modify` mkIfNode
-
--- -- Generate the Command for IF statements
--- mkIfNode :: ((((((String, Expr), String), Command), String), Command), String) -> Command
--- mkIfNode ((((((_, e), _), ifc), _), elsec), _) = Cond e ifc elsec
-
--- -- Parser for ASSIGNMENT statements
--- assign = (variable <&> literal ":=" <&> expr) `modify` mkAssignNode
-
--- -- Generate the Command for ASSIGNMENT statements
--- mkAssignNode :: ((Variable, String), Expr) -> Command
--- mkAssignNode ((v,_), e) = Assign v e
+-- Generate the Command for ASSIGNMENT statements
+mkAssignNode :: ((Variable, String), Expr) -> Command
+mkAssignNode ((v,_), e) = Assign v e
